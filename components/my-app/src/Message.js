c8c26253295201d0e712dc44036fb70c657b02fe
Eleonora <eleonoracasto93@gmail.com>
2023-02-22 18:01:40 +0100

    components 07

---
+++ components/my-app/src/Message.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class Message extends React.Component {
+    render() {
+        return (
+            < div>
+                <p>What a beautiful day!</p>
+            </div>
+        )
+    }
+}
+export default Message
