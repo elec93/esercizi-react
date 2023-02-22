96bcac6d68048f1df24abf58725b890387dfb19e
Eleonora <eleonoracasto93@gmail.com>
2023-02-22 15:41:52 +0100

    jsx

---
+++ JSX-01/my-app/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export class App extends React.Component {
+    render() {
+        const hello = <h1>Hello, World!</h1>;
+        return (
+            <div>
+              {hello}
+            </div>
+          );
+    }
+}
+
