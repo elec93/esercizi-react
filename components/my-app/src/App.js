c6f5d9c06d3eafd9d676276f0d513741d0c12545
Eleonora <eleonoracasto93@gmail.com>
2023-03-03 18:44:24 +0100

    events-03

--- components/my-app/src/App.js
+++ components/my-app/src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-import ClickCounter from "./ClickCounter";
+import ClickTracker from "./ClickTracker";
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <ClickCounter />
+        <ClickTracker />
       </div>
     );
   }
