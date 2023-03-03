3108d60e4ea14e508db8b6c6254eb4aa3a513e18
Eleonora <eleonoracasto93@gmail.com>
2023-03-03 11:22:53 +0100

    lifecycle 01

--- components/my-app/src/Counter.js
+++ components/my-app/src/Counter.js
@@ -4,15 +4,18 @@ import CounterDisplay from "./CounterDisplay";
 class Counter extends Component {
   state = { count: this.props.value };
 
-  constructor(props) {
-    super(props);
+  componentDidMount(){
     setInterval(() => {
       this.setState({ count: this.state.count + this.props.amount });
     }, this.props.interval);
   }
 
   render() {
-    return <h1><CounterDisplay count={this.state.count}/></h1>
+    return (
+      <h1>
+        <CounterDisplay count={this.state.count} />
+      </h1>
+    );
   }
 }
 
