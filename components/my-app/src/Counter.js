a4798aa22768eb13bd3c06db929861c7d755aace
Eleonora <eleonoracasto93@gmail.com>
2023-03-06 11:42:45 +0100

    fixed constructor

--- components/my-app/src/Counter.js
+++ components/my-app/src/Counter.js
@@ -1,17 +1,17 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = { count: this.props.value };
-
   constructor(props) {
     super(props);
+    this.state = { count: this.props.value };
+
     setInterval(() => {
       this.setState({ count: this.state.count + this.props.amount });
     }, this.props.interval);
   }
 
   render() {
-    return <h1>{this.state.count}</h1>
+    return <h1>{this.state.count}</h1>;
   }
 }
 
