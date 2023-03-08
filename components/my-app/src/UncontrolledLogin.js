1859dfbe2551066c821c4266296bd0c0036fb3a4
Eleonora <eleonoracasto93@gmail.com>
2023-03-08 15:47:44 +0100

    f

--- components/my-app/src/UncontrolledLogin.js
+++ components/my-app/src/UncontrolledLogin.js
@@ -1,22 +1,25 @@
 import React, { Component } from "react";
 
 class Login extends Component {
-  
+  ref = React.createRef();
+
   handleSubmit = (event) => {
     event.preventDefault();
-    const username = event.target.elements.username.value;
-    const password = event.target.elements.password.value;
-    const remember = event.target.elements.remember.checked;
+    const username = this.ref.current.elements.username.value;
+    const password = this.ref.current.elements.password.value;
+    const remember = this.ref.current.elements.remember.checked;
     this.props.onLogin({ username, password, remember });
   };
 
-  handleDisabled = (event) => {
-    const username = event.target.elements.username.value;
-    const password = event.target.elements.password.value;
-    return (!username || !password) ? true : false;
+  handleDisabled = () => {
+    const { username, password } = this.ref.current.elements;
+    return username.value === "" || password.value === "";
   };
 
   render() {
+
+
+
     return (
       <div>
         <h3>Uncontrolled Components</h3>
@@ -25,9 +28,7 @@ class Login extends Component {
           <input type="password" name="password" />
           <input type="checkbox" name="remember" />
 
-          <button type="submit" disabled={this.handleDisabled()}>
-            Login
-          </button>
+          <button type="submit" disabled={this.handleDisabled()}> Login </button>
           <button type="reset">Reset</button>
         </form>
       </div>
