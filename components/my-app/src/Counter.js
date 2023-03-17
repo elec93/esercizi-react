f4ebd3bad7fef6bfe6706481a27c249ba2bfdff4
Eleonora <eleonoracasto93@gmail.com>
2023-03-17 15:23:32 +0100

    effect 2

--- components/my-app/src/Counter.js
+++ components/my-app/src/Counter.js
@@ -1,24 +1,23 @@
-// Rewrite the Counter component from State 1 as a function component a
-// nd add a side effect that initializes the interval as soon as the component renders, 
+// Rewrite the Counter component from State 1 as a function component and
+// add a side effect that initializes the interval as soon as the component renders,
 // and clears it when the component unmounts.
 
 import React, { useState, useEffect } from "react";
 
-function Counter(){
-  
-    const [count, setCount] = useState(0);
+function Counter() {
+  const [count, setCount] = useState(0);
 
-    
-  useEffect(()=>{
+  useEffect(() => {
     const interval = setInterval(() => {
-      setCount( (prevState) => prevState + 1 );
+      setCount((prevState) => prevState + 1);
     }, 1000);
 
-    return ()=> {clearInterval(interval)}
+    return () => {
+      clearInterval(interval);
+    };
+  }, [count]);
 
-  },[count])
-  
-    return <h1>{count}</h1>;
+  return <h1>{count}</h1>;
 }
 
 export default Counter;
