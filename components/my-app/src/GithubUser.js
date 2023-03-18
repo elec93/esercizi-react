89f6ca16df30c0d2c9a8e37f1f705a1e12e92e02
Eleonora <eleonoracasto93@gmail.com>
2023-03-18 16:56:24 +0100

    useffect 3

--- components/my-app/src/GithubUser.js
+++ components/my-app/src/GithubUser.js
@@ -1,43 +1,38 @@
-// Create a GithubUser component that fetches the data of the username passed as a prop,
-// and renders some of the data within a div tag.
-// The API to query is https://api.github.com/users/${username}.
-import { useState, useEffect } from "react";
+// Create a Eliminare component that fetches the data of the username
+//  passed as a prop, and renders some of the data within a div tag.
+//  The API to query is https://api.github.com/users/${username}.
+
+import React, { useState, useEffect } from "react";
 
 function GithubUser({ username }) {
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const [user, setUser] = useState(null);
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    setLoading(true);
+    async function getData() {
+        try{
+            const response = await fetch(`https://api.github.com/users/${username}`);
+            const jsonData = await response.json();
+            setUser(jsonData);
+          }catch(error){
+            setError(error);
+          }
+        }  
     getData();
   }, [username]);
 
-  async function getData() {
-    try {
-      const response = await fetch(`https://api.github.com/users/${username}`);
-      const json = await response.json();
-      setData(json);
-    } catch (error) {
-      setError(error);
-    } finally {
-      setLoading(false);
-    }
+  if (error) {
+    return <div>There's an error: {error.message}</div>;
   }
 
-  if (error) {
-    return <div>Errore: {error.message}</div>;
+  if (!user) {
+    return <div>Loading...</div>;
   }
 
   return (
     <div>
-      {loading && <p>Loading...</p>}
-      {data && (
-        <div>
-          <h3>{data.name}</h3>
-          <img src={data.avatar_url} />
-        </div>
-      )}
+      <h4>{user.name}</h4>
+      <img src={user.avatar_url} />
     </div>
   );
 }
