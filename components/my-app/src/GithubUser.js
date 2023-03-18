6cfed80d01673877b26fe20855ba1e69f090b2c7
Eleonora <eleonoracasto93@gmail.com>
2023-03-18 17:07:39 +0100

    useffect 3

--- components/my-app/src/GithubUser.js
+++ components/my-app/src/GithubUser.js
@@ -1,7 +1,3 @@
-// Create a GithubUser component that fetches the data of the username
-//  passed as a prop, and renders some of the data within a div tag.
-//  The API to query is https://api.github.com/users/${username}.
-
 import React, { useState, useEffect } from "react";
 
 function GithubUser({ username }) {
