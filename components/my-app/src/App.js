131d8f1d8bd0f632844e24cb402c2950ba837d93
Eleonora <eleonoracasto93@gmail.com>
2023-03-24 15:44:39 +0100

    router 4

--- components/my-app/src/App.js
+++ components/my-app/src/App.js
@@ -1,18 +1,32 @@
-// Add a new Route to the users/:username path that renders a ShowGithubUser 
-// component that receives the username as a path parameter and renders 
-// the GithubUser component from useEffect 03 by passing it the received username.
+// Add three Links within the main App component and use them to navigate to all three routes.
 
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
+import Welcome from "./Welcome";
 import Counter from "./Counter";
 import ShowGithubUser from "./ShowGithubUser";
 
 function App() {
   return (
-    <Routes>
-      {/* <Route path="/counter" element={<Counter counter={0} />} /> */}
-      <Route path="/users/:username" element={<ShowGithubUser />} />
-    </Routes>
+    <>
+        <ul style={{display:'flex', flexDirection:'row', gap:20}}>
+          <li>
+            <Link to="/">Go to welcome</Link>
+          </li>
+          <li>
+            <Link to="/counter">Go to counter</Link>
+          </li>
+          <li>
+            <Link to="/users/:username">Go to user</Link>
+          </li>
+        </ul>
+  
+      <Routes>
+        <Route path="/" element={<Welcome name="Ele" />} />{" "}
+        <Route path="/counter" element={<Counter counter={0} />} />
+        <Route path="/users/:username" element={<ShowGithubUser />} />
+      </Routes>
+    </>
   );
 }
 
