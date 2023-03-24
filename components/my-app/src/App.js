d6640cee59241f59119eaabd913b74dbe1d0016a
Eleonora <eleonoracasto93@gmail.com>
2023-03-24 16:05:17 +0100

    router 5

--- components/my-app/src/App.js
+++ components/my-app/src/App.js
@@ -1,4 +1,4 @@
-// Add three Links within the main App component and use them to navigate to all three routes.
+// Add a Not Found route that renders when a user navigates to a path that does not exist.
 
 import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
@@ -9,11 +9,20 @@ import ShowGithubUser from "./ShowGithubUser";
 function App() {
   return (
     <>
-      <Link to="/">Go to welcome</Link> | <Link to="/counter">Go to counter</Link> | <Link to="/users/:username">Go to user</Link>
+      <Link to="/">Go to welcome</Link> |{" "}
+      <Link to="/counter">Go to counter</Link> |{" "}
+      <Link to="/users/:username">Go to user</Link>
       <Routes>
-        <Route path="/" element={<Welcome name="Ele" />} />{" "}
+        <Route path="/" element={<Welcome name="Ele" />} />
         <Route path="/counter" element={<Counter counter={0} />} />
-        <Route path="/users/:username" element={<ShowGithubUser />} />
+        {/* <Route path="/users/:username" element={<ShowGithubUser />} /> */}
+        <Route path="*" element={
+            <div>
+              <h3>NOT FOUND</h3>
+              <Link to="/">Go Back</Link>
+            </div>
+          }
+        />
       </Routes>
     </>
   );
